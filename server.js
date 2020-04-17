9ef278e9f97075767000818a27eedb492a96ec1f
tariktabakovic <tariktab97@gmail.com>
2020-04-17 16:15:08 -0400

    bring in http & socketio modules

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
