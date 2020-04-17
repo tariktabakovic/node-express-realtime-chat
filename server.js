cfa88accfa908deb59b4f5422db03eb268e7bd13
tariktabakovic <tariktab97@gmail.com>
2020-04-17 16:11:57 -0400

    sets up static folder using path

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000
 
