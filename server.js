85194edcf2964c68798b6721ff4d4a67455de617
tariktabakovic <tariktab97@gmail.com>
2020-04-19 17:46:52 -0400

    creates variable for botname, and implements it in welcoming the client

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,11 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord bot'
 // Run when client connects
 io.on('connection', socket =>{
     // Welcome client
-    socket.emit('message', 'Welcome to the Chat');
+    socket.emit('message', formatMessage(botName,'Welcome to the Chat'));
 
     // Broadcast when a user connects
     socket.broadcast.emit('message', 'A user has joined the chat');
